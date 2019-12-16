db22d7bcc085723d2d64bdea81ce3b50e2510b69
Your Name <you@example.com>
2019-12-17 01:30:05 +0800

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import {withStyles} from '@material-ui/core/styles';
 
-const style = theme =>({
+const styles = theme =>({
   root:{
     with: '100%',
     marginTop: theme.spacing.unit * 3,
@@ -18,32 +18,50 @@ const style = theme =>({
   table:{
     minWidth: 1080
   }
-})
+});
 
 class App extends React.Component{
-  render(){
-    const { classes } = this.props;
-    return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableCell>ID</TableCell>
-            <TableCell>IMG</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Birthday</TableCell>
-            <TableCell>Gender</TableCell>
-            <TableCell>Job</TableCell>
-          </TableHead>
-          <TableBody>
-            {customers.map(c => {
-              return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-            })}
-          </TableBody>
-        </Table>        
-      </Paper>
-    );
+
+  state = {
+  customers: ''
   }
   
-}
-
-export default withStyles(style)(App);
+  componentDidMount() {
+  this.callApi()
+  .then(res => this.setState({customers: res}))
+  .catch(err => console.log(err));
+  }
+  
+  callApi = async () => {
+  const response = await fetch('/api/customers');
+  const body = await response.json();
+  return body;
+  }
+  
+  render() {
+  const { classes } = this.props;
+  return (
+  <Paper className={classes.root}>
+  <Table className={classes.table}>
+  <TableHead>
+  <TableRow>
+  <TableCell>번호</TableCell>
+  <TableCell>이미지</TableCell>
+  <TableCell>이름</TableCell>
+  <TableCell>생년월일</TableCell>
+  <TableCell>성별</TableCell>
+  <TableCell>직업</TableCell>
+  </TableRow>
+  </TableHead>
+  <TableBody>
+  {this.state.customers ? this.state.customers.map(c => {
+  return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+  }) : ''}
+  </TableBody>
+  </Table>
+  </Paper>
+  );
+  }
+  }
+  
+  export default withStyles(styles)(App);
